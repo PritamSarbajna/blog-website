79f65cd4ab17467d83792e3de5b8eeff57bb7253
Pritam Sarbajna <pritamsarbajna10@gmail.com>
2022-06-18 13:19:18 +0530

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    title=models.CharField(max_length=200)
+    slug=models.SlugField()
+    
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = "Categories"
+        
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return '/%s/' % self.slug
+
+
+class Post(models.Model):
+    ACTIVE='active'
+    DRAFT='draft'
+    
+    CHOICES_STATUS=(
+        (ACTIVE, 'Active'),
+        (DRAFT, 'Draft')
+    )
+    category=models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    slug=models.SlugField()
+    intro=models.TextField()
+    body=models.TextField()
+    created_at=models.DateTimeField(default=datetime.now, blank=True)
+    status=models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
+    image=models.ImageField(upload_to='uploads/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ('-created_at',)
+        
+        
+    def get_absolute_url(self):
+        return '/%s/%s/' % (self.category.slug, self.slug)
+        
+class Comment(models.Model):
+    post=models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    body=models.TextField()
+    created_at=models.DateTimeField(default=datetime.now, blank=True)
+    
+    
+    def __str__(self):
+        return self.name
